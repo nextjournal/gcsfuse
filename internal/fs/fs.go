c658fee47a926d5138b5dec77be4b93a38a23a7f
Holger Amann <holger@nextjournal.com>
2019-05-27 15:24:27 +0200

    Disable sanity checks
    The sanity check here checks if the GCS object filename matches
    the requested filename and would fail if we map data-named/foo/s to
    data/foo

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -587,10 +587,14 @@ func (fs *fileSystem) mintInode(name string, o *gcs.Object) (in inode.Inode) {
 func (fs *fileSystem) lookUpOrCreateInodeIfNotStale(
 	name string,
 	o *gcs.Object) (in inode.Inode) {
-	// Sanity check.
-	if o != nil && name != o.Name {
+
+	// Sanity checks disabled because for requests to
+	// data-named the GCS object filename doesn't match
+	// the requested filename
+	// data-named/foo/s -> data/foo
+	/*if o != nil && name != o.Name {
 		panic(fmt.Sprintf("Name mismatch: %q vs. %q", name, o.Name))
-	}
+	}*/
 
 	// Ensure that no matter which inode we return, we increase its lookup count
 	// on the way out and then release the file system lock.
